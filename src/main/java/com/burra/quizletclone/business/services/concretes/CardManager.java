8fca5a83f9d36fdcf67022fde801ffbc4dffb4b5
Burra <gezerbugra@gmail.com>
2023-02-18 00:48:01 +0300

    error fixed

--- src/main/java/com/burra/quizletclone/business/services/concretes/CardManager.java
+++ src/main/java/com/burra/quizletclone/business/services/concretes/CardManager.java
@@ -1,9 +1,5 @@
 package com.burra.quizletclone.business.services.concretes;
 
-import java.util.ArrayList;
-
-import org.springframework.stereotype.Service;
-
 import com.burra.quizletclone.business.requests.cards.CardCreateRequest;
 import com.burra.quizletclone.business.services.abstracts.CardService;
 import com.burra.quizletclone.core.utilities.results.DataResult;
@@ -12,36 +8,41 @@ import com.burra.quizletclone.dataAccess.abstracts.CardRepository;
 import com.burra.quizletclone.dataAccess.abstracts.CardsetRepository;
 import com.burra.quizletclone.entities.concretes.Card;
 import com.burra.quizletclone.entities.concretes.Cardset;
+import java.util.ArrayList;
+import org.springframework.stereotype.Service;
 
 @Service
-public class CardManager implements CardService{
+public class CardManager implements CardService {
 
   CardRepository cardRepository;
   CardsetRepository cardsetRepository;
 
-  public CardManager(CardRepository cardRepository) {
+  public CardManager(
+    CardRepository cardRepository,
+    CardsetRepository cardsetRepository
+  ) {
     this.cardRepository = cardRepository;
+    this.cardsetRepository = cardsetRepository;
   }
 
-	@Override
-	public DataResult<ArrayList<Card>> getAll() {
-    ArrayList<Card> cards= (ArrayList<Card>) cardRepository.findAll();
-		return new SuccessDataResult<ArrayList<Card>>(cards);
-	}
+  @Override
+  public DataResult<ArrayList<Card>> getAll() {
+    ArrayList<Card> cards = (ArrayList<Card>) cardRepository.findAll();
+    return new SuccessDataResult<ArrayList<Card>>(cards);
+  }
 
-	@Override
-	public DataResult<Card> create(CardCreateRequest cardCreateRequest) {
-    Cardset cardset = cardsetRepository.getReferenceById(cardCreateRequest.getCardsetId());
+  @Override
+  public DataResult<Card> create(CardCreateRequest cardCreateRequest) {
+    Cardset cardset = cardsetRepository.getReferenceById(
+      cardCreateRequest.getCardsetId()
+    );
 
-    Card card = new Card(); 
+    Card card = new Card();
     card.setDefinition(cardCreateRequest.getDefinition());
     card.setTerm(cardCreateRequest.getTerm());
     card.setCardset(cardset);
     Card newCard = cardRepository.save(card);
-  
-		return new SuccessDataResult<Card>(newCard);
-	}
 
-
-  
+    return new SuccessDataResult<Card>(newCard);
+  }
 }
