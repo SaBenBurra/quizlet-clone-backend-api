3292677c33a026b2b1f84d9da60b28d85f02fafe
Burra <gezerbugra@gmail.com>
2023-02-18 00:48:19 +0300

    card create endpoint created

--- src/main/java/com/burra/quizletclone/webApi/CardController.java
+++ src/main/java/com/burra/quizletclone/webApi/CardController.java
@@ -1,9 +1,15 @@
 package com.burra.quizletclone.webApi;
 
+import com.burra.quizletclone.business.requests.cards.CardCreateRequest;
 import com.burra.quizletclone.business.services.abstracts.CardService;
 import com.burra.quizletclone.core.utilities.results.Result;
+
+import jakarta.validation.Valid;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +26,13 @@ public class CardController {
   @GetMapping("/getAll")
   public ResponseEntity<Result> getAll() {
     Result result = cardService.getAll();
+
+    return ResponseEntity.ok(result);
+  }
+
+  @PostMapping("/create")
+  public ResponseEntity<Result> create(@Valid @RequestBody CardCreateRequest request) {
+    Result result = cardService.create(request);
 
     return ResponseEntity.ok(result);
   }
