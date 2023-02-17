73df940418e4a2923926323de80f8ae34b7c990b
Burra <gezerbugra@gmail.com>
2023-02-17 21:48:51 +0300

    error fixed

--- src/main/java/com/burra/quizletclone/webApi/CardController.java
+++ src/main/java/com/burra/quizletclone/webApi/CardController.java
@@ -3,11 +3,11 @@ package com.burra.quizletclone.webApi;
 import com.burra.quizletclone.business.services.abstracts.CardService;
 import com.burra.quizletclone.core.utilities.results.Result;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Service
+@RestController
 @RequestMapping("/api/cards")
 public class CardController {
 
