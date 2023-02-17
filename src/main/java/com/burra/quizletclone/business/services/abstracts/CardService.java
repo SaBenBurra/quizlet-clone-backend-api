056ed9106893d742d4e8cb66a93570940d436152
Burra <gezerbugra@gmail.com>
2023-02-18 02:27:34 +0300

    update method created at CardService

--- src/main/java/com/burra/quizletclone/business/services/abstracts/CardService.java
+++ src/main/java/com/burra/quizletclone/business/services/abstracts/CardService.java
@@ -3,7 +3,9 @@ package com.burra.quizletclone.business.services.abstracts;
 import java.util.ArrayList;
 
 import com.burra.quizletclone.business.requests.cards.CardCreateRequest;
+import com.burra.quizletclone.business.requests.cards.CardUpdateRequest;
 import com.burra.quizletclone.business.responses.cards.CardCreateResponse;
+import com.burra.quizletclone.business.responses.cards.CardUpdateResponse;
 import com.burra.quizletclone.core.utilities.results.DataResult;
 import com.burra.quizletclone.core.utilities.results.Result;
 import com.burra.quizletclone.entities.concretes.Card;
@@ -18,4 +20,6 @@ public interface CardService {
   public DataResult<CardCreateResponse> create(CardCreateRequest request);
 
   public Result delete(int cardId);
+
+  public DataResult<CardUpdateResponse> update(CardUpdateRequest request, int cardId);
 }
