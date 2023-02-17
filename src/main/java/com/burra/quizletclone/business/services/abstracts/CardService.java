eca86f294b9e9f40c2667ba0324f7279f7ee8761
Burra <gezerbugra@gmail.com>
2023-02-18 01:32:16 +0300

    create method updated at CardService

--- src/main/java/com/burra/quizletclone/business/services/abstracts/CardService.java
+++ src/main/java/com/burra/quizletclone/business/services/abstracts/CardService.java
@@ -3,6 +3,7 @@ package com.burra.quizletclone.business.services.abstracts;
 import java.util.ArrayList;
 
 import com.burra.quizletclone.business.requests.cards.CardCreateRequest;
+import com.burra.quizletclone.business.responses.cards.CardCreateResponse;
 import com.burra.quizletclone.core.utilities.results.DataResult;
 import com.burra.quizletclone.core.utilities.results.Result;
 import com.burra.quizletclone.entities.concretes.Card;
@@ -14,7 +15,7 @@ public interface CardService {
 
   public DataResult<ArrayList<Card>> getAll();
 
-  public DataResult<Card> create(CardCreateRequest request);
+  public DataResult<CardCreateResponse> create(CardCreateRequest request);
 
   public Result delete(int cardId);
 }
