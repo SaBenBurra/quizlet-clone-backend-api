47e12069543225b99f877e6d0ddfd21a834297a9
Burra <gezerbugra@gmail.com>
2023-02-17 16:50:54 +0300

    initial commit

---
+++ src/test/java/com/burra/quizletclone/QuizletcloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.burra.quizletclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizletcloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
