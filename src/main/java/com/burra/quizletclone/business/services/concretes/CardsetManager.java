5a35bd026f3b991f4b921c57acef6979df6b3fc2
Burra <gezerbugra@gmail.com>
2023-03-30 00:57:29 +0300

    update method updated at CardsetManager

--- src/main/java/com/burra/quizletclone/business/services/concretes/CardsetManager.java
+++ src/main/java/com/burra/quizletclone/business/services/concretes/CardsetManager.java
@@ -1,18 +1,24 @@
 package com.burra.quizletclone.business.services.concretes;
 
+import com.burra.quizletclone.business.requests.cards.CardCreateRequest;
 import com.burra.quizletclone.business.requests.cardsets.CardsetCreateRequest;
 import com.burra.quizletclone.business.requests.cardsets.CardsetUpdateRequest;
 import com.burra.quizletclone.business.responses.cardsets.CardsetCreateResponse;
 import com.burra.quizletclone.business.responses.cardsets.CardsetGetAllResponse;
 import com.burra.quizletclone.business.responses.cardsets.CardsetGetByIdResponse;
 import com.burra.quizletclone.business.responses.cardsets.CardsetUpdateResponse;
+import com.burra.quizletclone.business.services.abstracts.CardService;
 import com.burra.quizletclone.business.services.abstracts.CardsetService;
 import com.burra.quizletclone.core.utilities.results.DataResult;
 import com.burra.quizletclone.core.utilities.results.Result;
 import com.burra.quizletclone.core.utilities.results.SuccessDataResult;
 import com.burra.quizletclone.core.utilities.results.SuccessResult;
 import com.burra.quizletclone.dataAccess.abstracts.CardsetRepository;
+import com.burra.quizletclone.entities.concretes.Card;
 import com.burra.quizletclone.entities.concretes.Cardset;
+
+import jakarta.transaction.Transactional;
+
 import java.util.ArrayList;
 import org.springframework.stereotype.Service;
 
@@ -20,9 +26,11 @@ import org.springframework.stereotype.Service;
 public class CardsetManager implements CardsetService {
 
   private CardsetRepository cardsetRepository;
+  private CardService cardService;
 
-  public CardsetManager(CardsetRepository cardsetRepository) {
+  public CardsetManager(CardsetRepository cardsetRepository, CardService cardService) {
     this.cardsetRepository = cardsetRepository;
+    this.cardService = cardService;
   }
 
   @Override
@@ -69,12 +77,28 @@ public class CardsetManager implements CardsetService {
   }
 
   @Override
+  @Transactional
   public DataResult<CardsetUpdateResponse> update(
     CardsetUpdateRequest request,
     int cardsetId
   ) {
     Cardset cardset = cardsetRepository.getReferenceById(cardsetId);
     cardset.setName(request.getName());
+
+    ArrayList<Card> oldCards = (ArrayList<Card>) cardset.getCards();
+    for(Card card : oldCards) {
+      cardService.delete(card.getId());
+    }
+
+    ArrayList<CardCreateRequest> newCardsData = (ArrayList<CardCreateRequest>) request.getCards();
+    
+    for(CardCreateRequest cardData : newCardsData) {
+      Card newCard = new Card();
+      newCard.setDefinition(cardData.getDefinition());
+      newCard.setTerm(cardData.getTerm());
+      cardset.addCard(newCard);
+    }
+
     Cardset updatedCardset = cardsetRepository.save(cardset);
 
     CardsetUpdateResponse response = CardsetUpdateResponse.FromEntity(
