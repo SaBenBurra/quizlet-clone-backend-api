04ecb129cafb30c5ef8fa6f812ebdbd259cce316
Burra <gezerbugra@gmail.com>
2023-03-30 00:56:25 +0300

    CardsetUpdateRequest updated

--- src/main/java/com/burra/quizletclone/business/requests/cardsets/CardsetUpdateRequest.java
+++ src/main/java/com/burra/quizletclone/business/requests/cardsets/CardsetUpdateRequest.java
@@ -1,10 +1,20 @@
 package com.burra.quizletclone.business.requests.cardsets;
 
+import java.util.List;
+
+import com.burra.quizletclone.business.requests.cards.CardCreateRequest;
+
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 
 @Data
 public class CardsetUpdateRequest {
   @NotBlank
   private String name;
+
+  @Valid
+  @NotEmpty
+  private List<CardCreateRequest> cards;
 }
