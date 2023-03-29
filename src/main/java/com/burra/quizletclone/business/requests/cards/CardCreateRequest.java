c0458820883e03d64c7fe726e2a6df33d21bac47
Burra <gezerbugra@gmail.com>
2023-03-30 00:48:08 +0300

    cardCreateRequest updated

--- src/main/java/com/burra/quizletclone/business/requests/cards/CardCreateRequest.java
+++ src/main/java/com/burra/quizletclone/business/requests/cards/CardCreateRequest.java
@@ -1,17 +1,10 @@
 package com.burra.quizletclone.business.requests.cards;
 
-import com.burra.quizletclone.core.annotations.Exists;
-
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class CardCreateRequest {
-
-  @NotNull
-  @Exists(table = "cardsets")
-  private int cardsetId;
 
   @NotBlank
   private String definition;
