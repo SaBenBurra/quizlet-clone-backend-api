ab2aa02607ae6b25cae9540afa2ee885565e1b6d
Burra <gezerbugra@gmail.com>
2023-04-13 14:08:59 +0300

    cardset manager updated

--- src/main/java/com/burra/quizletclone/business/services/concretes/CardsetManager.java
+++ src/main/java/com/burra/quizletclone/business/services/concretes/CardsetManager.java
@@ -17,9 +17,7 @@ import com.burra.quizletclone.dataAccess.abstracts.CardRepository;
 import com.burra.quizletclone.dataAccess.abstracts.CardsetRepository;
 import com.burra.quizletclone.entities.concretes.Card;
 import com.burra.quizletclone.entities.concretes.Cardset;
-
 import jakarta.transaction.Transactional;
-
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.stereotype.Service;
@@ -31,7 +29,9 @@ public class CardsetManager implements CardsetService {
   private CardRepository cardRepository;
   private CardService cardService;
 
-  public CardsetManager(CardsetRepository cardsetRepository, CardRepository cardRepository, CardService cardService) {
+  public CardsetManager(CardsetRepository cardsetRepository,
+                        CardRepository cardRepository,
+                        CardService cardService) {
     this.cardsetRepository = cardsetRepository;
     this.cardRepository = cardRepository;
     this.cardService = cardService;
@@ -39,39 +39,36 @@ public class CardsetManager implements CardsetService {
 
   @Override
   public DataResult<ArrayList<CardsetGetAllResponse>> getAll() {
-    ArrayList<Cardset> cardsets = (ArrayList<Cardset>) cardsetRepository.findAll();
-    ArrayList<CardsetGetAllResponse> response = new ArrayList<CardsetGetAllResponse>();
+    ArrayList<Cardset> cardsets =
+        (ArrayList<Cardset>)cardsetRepository.findAllByOrderByIdDesc();
+    ArrayList<CardsetGetAllResponse> response =
+        new ArrayList<CardsetGetAllResponse>();
 
     for (Cardset cardset : cardsets) {
       response.add(CardsetGetAllResponse.FromEntity(cardset));
     }
 
     return new SuccessDataResult<ArrayList<CardsetGetAllResponse>>(
-      response,
-      "Setler listelendi."
-    );
+        response, "Setler listelendi.");
   }
 
   @Override
   public DataResult<CardsetGetByIdResponse> getById(int cardsetId) {
     Cardset cardset = cardsetRepository.getReferenceById(cardsetId);
-    CardsetGetByIdResponse response = CardsetGetByIdResponse.FromEntity(
-      cardset
-    );
+    CardsetGetByIdResponse response =
+        CardsetGetByIdResponse.FromEntity(cardset);
     return new SuccessDataResult<CardsetGetByIdResponse>(response);
   }
 
   @Override
-  public DataResult<CardsetCreateResponse> create(
-    CardsetCreateRequest request
-  ) {
+  public DataResult<CardsetCreateResponse>
+  create(CardsetCreateRequest request) {
     Cardset cardset = new Cardset();
     cardset.setName(request.getName());
 
     Cardset newCardset = cardsetRepository.save(cardset);
     return new SuccessDataResult<CardsetCreateResponse>(
-      CardsetCreateResponse.FromEntity(newCardset)
-    );
+        CardsetCreateResponse.FromEntity(newCardset));
   }
 
   @Override
@@ -82,23 +79,22 @@ public class CardsetManager implements CardsetService {
 
   @Override
   @Transactional
-  public DataResult<CardsetUpdateResponse> update(
-    CardsetUpdateRequest request,
-    int cardsetId
-  ) {
+  public DataResult<CardsetUpdateResponse> update(CardsetUpdateRequest request,
+                                                  int cardsetId) {
     Cardset cardset = cardsetRepository.findById(cardsetId).get();
     cardset.setName(request.getName());
 
     List<Card> oldCards = cardset.getCards();
 
-    for(Card card : oldCards) {
+    for (Card card : oldCards) {
       cardRepository.delete(card);
     }
     oldCards.clear();
 
-    ArrayList<CardCreateRequest> newCardsData = (ArrayList<CardCreateRequest>) request.getCards();
-    
-    for(CardCreateRequest cardData : newCardsData) {
+    ArrayList<CardCreateRequest> newCardsData =
+        (ArrayList<CardCreateRequest>)request.getCards();
+
+    for (CardCreateRequest cardData : newCardsData) {
       Card newCard = new Card();
       newCard.setDefinition(cardData.getDefinition());
       newCard.setTerm(cardData.getTerm());
@@ -108,12 +104,11 @@ public class CardsetManager implements CardsetService {
 
     Cardset updatedCardset = cardsetRepository.save(cardset);
 
-    for(Card card : updatedCardset.getCards()) {
+    for (Card card : updatedCardset.getCards()) {
       System.out.println(card.getDefinition() + "--" + card.getTerm());
     }
-    CardsetUpdateResponse response = CardsetUpdateResponse.FromEntity(
-      updatedCardset
-    );
+    CardsetUpdateResponse response =
+        CardsetUpdateResponse.FromEntity(updatedCardset);
 
     return new SuccessDataResult<CardsetUpdateResponse>(response);
   }
