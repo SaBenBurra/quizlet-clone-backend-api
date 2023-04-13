d465dcc74c0d651d358e3e81d225a84f89fc63a8
Burra <gezerbugra@gmail.com>
2023-04-13 14:08:46 +0300

    order by id method defined for Cardset entity in repository

--- src/main/java/com/burra/quizletclone/dataAccess/abstracts/CardsetRepository.java
+++ src/main/java/com/burra/quizletclone/dataAccess/abstracts/CardsetRepository.java
@@ -1,6 +1,9 @@
 package com.burra.quizletclone.dataAccess.abstracts;
 
 import com.burra.quizletclone.entities.concretes.Cardset;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CardsetRepository extends JpaRepository<Cardset, Integer> {}
+public interface CardsetRepository extends JpaRepository<Cardset, Integer> {
+  List<Cardset> findAllByOrderByIdDesc();
+}
